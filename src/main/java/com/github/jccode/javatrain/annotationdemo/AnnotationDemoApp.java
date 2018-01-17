87ba238340f7dc3f667c99ff013ebcd2e2df418e
jcchen <junchangchen@gmail.com>
2018-01-18 02:22:50 +0800

    annotation demo

--- src/main/java/com/github/jccode/javatrain/annotationdemo/AnnotationDemoApp.java
+++ src/main/java/com/github/jccode/javatrain/annotationdemo/AnnotationDemoApp.java
@@ -1,8 +1,13 @@
 package com.github.jccode.javatrain.annotationdemo;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.lang.reflect.Method;
 
 @SpringBootApplication
 public class AnnotationDemoApp {
@@ -11,5 +16,18 @@ public class AnnotationDemoApp {
         SpringApplication.run(AnnotationDemoApp.class, args);
     }
 
+    @Autowired
+    private SampleHandlers sampleHandlers;
+
+
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return args -> {
+            Method[] methods = SampleHandlers.class.getDeclaredMethods();
+            System.out.println("invoke");
+            System.out.println(methods[0]);
+            methods[0].invoke(sampleHandlers);
+        };
+    }
 
 }
